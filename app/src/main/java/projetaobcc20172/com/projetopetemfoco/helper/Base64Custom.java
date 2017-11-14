bb48c9dec65b388f312cd4d92f4637c3567d5270
dariofrazao <dario.frazao@hotmail.com>
2017-11-14 16:54:51 -0300

    Adição de comentários gerais no código

--- app/src/main/java/projetaobcc20172/com/projetopetemfoco/helper/Base64Custom.java
+++ app/src/main/java/projetaobcc20172/com/projetopetemfoco/helper/Base64Custom.java
@@ -2,13 +2,13 @@ package projetaobcc20172.com.projetopetemfoco.helper;
 
 import android.util.Base64;
 
-public class Base64Custom {
+public class Base64Custom { //Classe para codificação e decodificação de texto
 
-    public static String codificarBase64(String texto){
+    public static String codificarBase64(String texto){ //Método para codificar texto na Base64
         return Base64.encodeToString(texto.getBytes(), Base64.DEFAULT).replaceAll("(\\n|\\r)","");
     }
 
-    public static String decodificarBase64(String textoCodificado){
+    public static String decodificarBase64(String textoCodificado){ //Método para decodificar texto da Base64
         return new String( Base64.decode(textoCodificado, Base64.DEFAULT ) );
     }
 

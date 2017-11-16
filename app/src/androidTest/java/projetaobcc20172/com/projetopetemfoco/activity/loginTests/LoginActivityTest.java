a04e721a2fd517914ab31f11ccf1fac1cdb99b4d
Darkkyelfo <raulpedrouag@gmail.com>
2017-11-16 04:03:35 -0300

    Classes de teste refatoradas.

--- app/src/androidTest/java/projetaobcc20172/com/projetopetemfoco/activity/loginTests/LoginActivityTest.java
+++ app/src/androidTest/java/projetaobcc20172/com/projetopetemfoco/activity/loginTests/LoginActivityTest.java
@@ -1,4 +1,4 @@
-package projetaobcc20172.com.projetopetemfoco.activity;
+package projetaobcc20172.com.projetopetemfoco.activity.loginTests;
 
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.action.ViewActions;
@@ -12,8 +12,9 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import projetaobcc20172.com.projetopetemfoco.R;
+import projetaobcc20172.com.projetopetemfoco.activity.LoginActivity;
+import projetaobcc20172.com.projetopetemfoco.activity.MainActivity;
 
-import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
@@ -37,39 +38,33 @@ public class LoginActivityTest{
     @Rule
     public ActivityTestRule<LoginActivity> loginActivityRule = new ActivityTestRule<>(LoginActivity.class);
 
+    public void preencherEClicar(String login,String senha){
+        //preenche o campo de e-mail com o texto do "loginInvalido"
+        Espresso.onView(ViewMatchers.withId(R.id.editText_email)).perform(ViewActions.typeText(login));
+        //Fecha o teclado
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(ViewMatchers.withId(R.id.editText_senha)).perform(ViewActions.typeText(senha));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(ViewMatchers.withId(R.id.botao_login)).perform(ViewActions.click());
+    }
+
     @Before
     public void setUp() throws Exception {
 
     }
-    //método que apaga o conteudo das caixas de texto
-    //Não é um teste é só pra facilitar a execução dos teste sequencialmente
-    @Test
-    public void limparCampos(){
-        Espresso.onView(ViewMatchers.withId(R.id.editText_email)).perform(clearText());
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(ViewMatchers.withId(R.id.editText_senha)).perform(clearText());
-        Espresso.closeSoftKeyboard();
-        loginActivityRule.getActivity().getToast().cancel();
-    }
+
     //Teste que simula uma tentatica de login com um cadastro não existente
     @Test
     public void testeLoginNaoCadastrado() {
-        //preenche o campo de e-mail com o texto do "loginInvalido"
-        Espresso.onView(ViewMatchers.withId(R.id.editText_email)).perform(ViewActions.typeText(this.loginInvalido));
-        //Fecha o teclado
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(ViewMatchers.withId(R.id.editText_senha)).perform(ViewActions.typeText(this.senhaInvalida));
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(ViewMatchers.withId(R.id.botao_login)).perform(ViewActions.click());
+        this.preencherEClicar(this.loginInvalido,this.senhaInvalida);
         Espresso.onView(ViewMatchers.withText(R.string.erro_login_invalido_Toast)).inRoot(withDecorView(not(is(loginActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
-        this.limparCampos();
     }
     //Teste que simula um login com todos os campos de texto em branco
     @Test
     public void testeCamposEmBranco(){
         Espresso.onView(ViewMatchers.withId(R.id.botao_login)).perform(ViewActions.click());
         Espresso.onView(ViewMatchers.withText(R.string.erro_login_invalido_Toast)).inRoot(withDecorView(not(is(loginActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
-        this.limparCampos();
+
     }
     //Teste que simula um login com o campo de senha em branco
     @Test
@@ -78,45 +73,27 @@ public class LoginActivityTest{
         Espresso.closeSoftKeyboard();
         Espresso.onView(ViewMatchers.withId(R.id.botao_login)).perform(ViewActions.click());
         Espresso.onView(ViewMatchers.withText(R.string.erro_login_invalido_Toast)).inRoot(withDecorView(not(is(loginActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
-        this.limparCampos();
+
     }
 
     @Test
     public void testeSenhaInvalida(){
-        Espresso.onView(ViewMatchers.withId(R.id.editText_email)).perform(ViewActions.typeText(this.loginValido));
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(ViewMatchers.withId(R.id.editText_senha)).perform(ViewActions.typeText(this.senhaInvalida));
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(ViewMatchers.withId(R.id.botao_login)).perform(ViewActions.click());
+        this.preencherEClicar(this.loginValido,this.senhaInvalida);
         Espresso.onView(ViewMatchers.withText(R.string.erro_login_invalido_Toast)).inRoot(withDecorView(not(is(loginActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
-        this.limparCampos();
     }
+
     @Test
     public void testeUsuarioCadastrado(){
         Intents.init();
-        Espresso.onView(ViewMatchers.withId(R.id.editText_email)).perform(ViewActions.typeText(this.loginValido));
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(ViewMatchers.withId(R.id.editText_senha)).perform(ViewActions.typeText(this.senhaValida));
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(ViewMatchers.withId(R.id.botao_login)).perform(ViewActions.click());
+        this.preencherEClicar(this.loginValido,this.senhaValida);
         Espresso.onView(ViewMatchers.withText(R.string.sucesso_login_Toast)).inRoot(withDecorView(not(is(loginActivityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
         //Verifica se houve mudança de activity(mudou a tela)
         intended(hasComponent(MainActivity.class.getName()));
     }
 
-    //Executa todos os testes criados anteriormente
-    @Test
-    public void realizarTestes(){
-        this.testeLoginNaoCadastrado();
-        this.testeCamposEmBranco();
-        this.testeSenhaEmBranco();
-        this.testeSenhaInvalida();
-        this.testeUsuarioCadastrado();
-    }
-
     @After
     public void tearDown() throws Exception {
-        Thread.sleep(5000);
+       // Thread.sleep(5000);
     }
 
 }

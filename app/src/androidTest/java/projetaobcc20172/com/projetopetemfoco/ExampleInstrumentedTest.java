1283476895a788599cd4bf3299ea08c3ba88df34
Darkkyelfo <raulpedrouag@gmail.com>
2017-11-21 17:47:46 -0300

    configuracao travis atualizado

--- app/src/androidTest/java/projetaobcc20172/com/projetopetemfoco/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/projetaobcc20172/com/projetopetemfoco/ExampleInstrumentedTest.java
@@ -20,7 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("projetaobcc20172.com.petemfoco", appContext.getPackageName());
     }
 }

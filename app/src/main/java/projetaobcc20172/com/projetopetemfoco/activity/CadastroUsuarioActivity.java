bf2dbb8a91e254f9f2150e0185a0d753e52156bd
dariofrazao <dario.frazao@hotmail.com>
2017-11-22 03:10:31 -0300

    Inclusão da logo na tela de login e inclusão de Toolbar de navegação entre telas Refatorado.

--- app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/CadastroUsuarioActivity.java
@@ -2,16 +2,13 @@ package projetaobcc20172.com.projetopetemfoco.activity;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;

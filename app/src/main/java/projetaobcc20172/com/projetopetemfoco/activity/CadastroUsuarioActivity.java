73d4a45374b5df9df931d5cdc42cc0c3932e59fe
Raul Sousa <raulpedrouag@gmail.com>
2017-11-23 18:18:52 -0300

    Inclusão do teste de aceitação de login

--- app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/CadastroUsuarioActivity.java
@@ -1,10 +1,12 @@
 package projetaobcc20172.com.projetopetemfoco.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,6 +41,8 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
 
+        Toolbar toolbar;
+        toolbar = (Toolbar) findViewById(R.id.tb_cadastro);
         nome = findViewById(R.id.editText_nome);
         email = findViewById(R.id.editText_email);
         senha = findViewById(R.id.editText_senha);
@@ -55,6 +59,20 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
                 cadastrarUsuario();
             }
         });
+
+        // Configura toolbar
+        toolbar.setTitle("Cadastro de Usuário");
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setNavigationIcon(R.drawable.ic_action_arrow_left_white);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
     //Verifica se a senha 1 é igual a senha 2
     private void verificarSenhar() throws SenhasDiferentesException {

7ab4408c1073af0fd6b63211ac186d862e9c2b1c
alexsandroms <alexsandroms@gmail.com>
2017-11-23 22:10:38 -0300

    Refatoraçao do tratamento pra não permitir os campos de nome, email e senhas vazias, além de verificar se as senhas são diferentes.

--- app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/CadastroUsuarioActivity.java
@@ -49,11 +49,36 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
         botaoCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                usuario = new Usuario();
-                usuario.setNome( nome.getText().toString() );
-                usuario.setEmail(email.getText().toString());
-                usuario.setSenha(senha.getText().toString());
-                cadastrarUsuario();
+                //Verifica se o campo nome esta vazio
+                if(!nome.getText().toString().isEmpty()) {
+                    //Verifica se o campo email esta vazio
+                    if (!email.getText().toString().isEmpty()) {
+                        //Verifica se campo senha esta vazio
+                        if (!senha.getText().toString().isEmpty()) {
+                            //Verifica se campo senha2 esta vazio
+                            if (!senha2.getText().toString().isEmpty()) {
+                                //Verifica se as senhas são iguais
+                                if (senha.getText().toString().equals(senha2.getText().toString())) {
+                                    usuario = new Usuario();
+                                    usuario.setNome(nome.getText().toString());
+                                    usuario.setEmail(email.getText().toString());
+                                    usuario.setSenha(senha.getText().toString());
+                                    cadastrarUsuario();
+                                } else {
+                                    Toast.makeText(CadastroUsuarioActivity.this, "As senhas devem ser iguais", Toast.LENGTH_LONG).show();
+                                }
+                            } else {
+                                Toast.makeText(CadastroUsuarioActivity.this, "A Senha de confirmação deve ser preenchida", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(CadastroUsuarioActivity.this, "A Senha deve ser preenchida", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(CadastroUsuarioActivity.this, "O E-mail deve ser preenchido", Toast.LENGTH_LONG).show();
+                    }
+                }else{
+                    Toast.makeText(CadastroUsuarioActivity.this, "O Nome deve ser preenchido", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -82,52 +107,40 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
     }
 
 
-
     private void cadastrarUsuario() {
-        if (email.getText().toString() != "") {
-            if (nome.getText().toString() != "") {
-                if (senha.getText().toString().equals(senha2.getText().toString())) {
-                    autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
-                    autenticacao.createUserWithEmailAndPassword(
-                            usuario.getEmail(),
-                            usuario.getSenha()
-                    ).addOnCompleteListener(CadastroUsuarioActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(CadastroUsuarioActivity.this, "Sucesso ao cadastrar usuário", Toast.LENGTH_LONG).show();
-                                String identificadorUsuario = Base64Custom.codificarBase64(usuario.getEmail());
-                                usuario.setId(identificadorUsuario);
-                                usuario.salvar();
-                                Preferencias preferencias = new Preferencias(CadastroUsuarioActivity.this);
-                                preferencias.salvarDados(identificadorUsuario, usuario.getNome());
-                                abrirLoginUsuario();
-                            } else {
-                                String erro = "";
-                                try {
-                                    throw task.getException();
-                                } catch (FirebaseAuthWeakPasswordException e) {
-                                    erro = "Escolha uma senha que contenha, letras e números.";
-                                } catch (FirebaseAuthInvalidCredentialsException e) {
-                                    erro = "Email indicado não é válido.";
-                                } catch (FirebaseAuthUserCollisionException e) {
-                                    erro = "Já existe uma conta com esse e-mail.";
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                                Toast.makeText(CadastroUsuarioActivity.this, "Erro ao cadastrar usuário: " + erro, Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(CadastroUsuarioActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(CadastroUsuarioActivity.this, "Sucesso ao cadastrar usuário", Toast.LENGTH_LONG).show();
+                    String identificadorUsuario = Base64Custom.codificarBase64(usuario.getEmail());
+                    usuario.setId(identificadorUsuario);
+                    usuario.salvar();
+                    Preferencias preferencias = new Preferencias(CadastroUsuarioActivity.this);
+                    preferencias.salvarDados(identificadorUsuario, usuario.getNome());
+                    abrirLoginUsuario();
                 } else {
-                    Toast.makeText(CadastroUsuarioActivity.this, "As senhas devem ser iguais", Toast.LENGTH_LONG).show();
+                    String erro = "";
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthWeakPasswordException e) {
+                        erro = "Escolha uma senha que contenha, letras e números.";
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        erro = "Email indicado não é válido.";
+                    } catch (FirebaseAuthUserCollisionException e) {
+                        erro = "Já existe uma conta com esse e-mail.";
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(CadastroUsuarioActivity.this, "Erro ao cadastrar usuário: " + erro, Toast.LENGTH_LONG).show();
                 }
-            } else {
-                Toast.makeText(CadastroUsuarioActivity.this, "O Nome deve ser preenchido", Toast.LENGTH_LONG).show();
             }
-        } else {
-            Toast.makeText(CadastroUsuarioActivity.this, "O E-mail deve ser preenchido", Toast.LENGTH_LONG).show();
-        }
+        });
+
     }
 
     public void abrirLoginUsuario(){

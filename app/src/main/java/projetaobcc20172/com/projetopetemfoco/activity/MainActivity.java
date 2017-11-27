3912f2764d8ed5757172d0560e7ce7d9c9055b31
dariofrazao <dario.frazao@hotmail.com>
2017-11-27 16:35:55 -0300

    Adicionado função para deslogar usuário na tela principal

--- app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/MainActivity.java
+++ app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/MainActivity.java
@@ -1,14 +1,44 @@
 package projetaobcc20172.com.projetopetemfoco.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import projetaobcc20172.com.projetopetemfoco.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth usuarioFirebase;
+    private Button sair;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        sair = (Button)findViewById(R.id.botao_sair);
+
+        sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deslogarUsuario();
+            }
+        });
+    }
+
+    private void deslogarUsuario(){
+
+        usuarioFirebase.signOut();
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }

da704a5a6510063037ee18274e8bc5f0a37af789
Felipe <flpdias14@gmail.com>
2017-12-08 15:21:46 -0300

    Update CadastroEnderecoActivity.java
    Remoção import não utilizado

--- app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/CadastroEnderecoActivity.java
+++ app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/CadastroEnderecoActivity.java
@@ -6,7 +6,6 @@ package projetaobcc20172.com.projetopetemfoco.activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;

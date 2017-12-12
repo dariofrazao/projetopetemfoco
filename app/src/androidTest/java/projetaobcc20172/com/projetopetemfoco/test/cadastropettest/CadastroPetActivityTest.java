d75434a8684dad699ecd0f05dd5269aaf319cf4f
Darkkyelfo <raulpedrouag@gmail.com>
2017-12-12 03:33:57 -0300

    Add login antes de realizar teste de aceitação de cadastrar pet.

--- app/src/androidTest/java/projetaobcc20172/com/projetopetemfoco/test/cadastropettest/CadastroPetActivityTest.java
+++ app/src/androidTest/java/projetaobcc20172/com/projetopetemfoco/test/cadastropettest/CadastroPetActivityTest.java
@@ -1,16 +1,20 @@
 package projetaobcc20172.com.projetopetemfoco.test.cadastropettest;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import projetaobcc20172.com.projetopetemfoco.R;
 import projetaobcc20172.com.projetopetemfoco.activity.CadastroPetActivity;
+import projetaobcc20172.com.projetopetemfoco.activity.LoginActivity;
 import projetaobcc20172.com.projetopetemfoco.activity.MainActivity;
 import projetaobcc20172.com.projetopetemfoco.test.TestTools;
+import projetaobcc20172.com.projetopetemfoco.test.logintests.LoginActivityTest;
 
 /**
  * Created by dario on 07/12/17.
@@ -23,8 +27,27 @@ public class CadastroPetActivityTest{
     private static String sNomePet = "Billie";
     private static String sraca = "Bulldog";
 
+    /*
     @Rule
     public ActivityTestRule<CadastroPetActivity> cadPetActivityRule = new ActivityTestRule<>(CadastroPetActivity.class);
+    */
+    @Rule
+    public ActivityTestRule <LoginActivity> loginActivityRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        try{
+            TestTools.clicarBotao(R.id.botao_sair);
+        }catch (Exception e){
+            e.getMessage();
+        }
+
+        LoginActivityTest log = new LoginActivityTest();
+        log.testeUsuarioCadastrado();
+        TestTools.clicarBotao(R.id.botao_cadastrar_pet);
+        Espresso.closeSoftKeyboard();
+        Thread.sleep(3000);
+    }
 
     //Teste que simula um cadastro do pet com o campo nome em branco
     @Test

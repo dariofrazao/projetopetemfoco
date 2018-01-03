15dd7cb2614cdc147fec45a78749d1fa77bdce15
Raul Sousa <raulpedrouag@gmail.com>
2018-01-02 21:56:57 -0300

    Add classes de fornecedor e serviço assim como seus daos ao projeto consumidor

---
+++ app/src/main/java/projetaobcc20172/com/projetopetemfoco/model/Servico.java
@@ -0,0 +1,67 @@
+package projetaobcc20172.com.projetopetemfoco.model;
+
+import com.google.firebase.database.Exclude;
+
+/**
+ * Created by Felipe Oliveira on 05/12/17.
+ *
+ */
+
+public class Servico {
+    private String mId;
+    private String mNome;
+    private String mDescricao;
+    private String mValor;
+    private String mTipoPet;
+
+    public Servico() {}
+
+    public Servico(String nome, String valor,String tipoPet){
+        this.mNome = nome;
+        this.mValor = valor;
+        this.mTipoPet = tipoPet;
+    }
+
+    @Exclude
+    public String getId() {
+        return mId;
+    }
+
+    public String getNome() {
+        return mNome;
+    }
+
+    public void setNome(String nome) {
+        this.mNome = nome;
+    }
+
+    public String getDescricao() {
+        return mDescricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.mDescricao = descricao;
+    }
+
+    public String getValor() {
+        return mValor;
+    }
+
+    public void setValor(String Valor) {
+        this.mValor = Valor;
+    }
+/*
+    public void salvar(String idEstabelecimento){
+        DatabaseReference referenciaFirebase = ConfiguracaoFirebase.getFirebase();
+        referenciaFirebase.child("fornecedor").child( idEstabelecimento ).setValue(this);
+    }
+    */
+
+    public String getTipoPet() {
+        return mTipoPet;
+    }
+
+    public void setTipoPet(String mTipoPet) {
+        this.mTipoPet = mTipoPet;
+    }
+}

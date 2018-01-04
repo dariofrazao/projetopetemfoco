07781a7c8e90346cbaca56ab8b0fd6d3620325f6
Raul Sousa <raulpedrouag@gmail.com>
2018-01-04 00:21:08 -0300

    navigation Menu criado para buscas

---
+++ app/src/main/java/projetaobcc20172/com/projetopetemfoco/activity/BuscaEstabelecimentoActivity.java
@@ -0,0 +1,33 @@
+package projetaobcc20172.com.projetopetemfoco.activity;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import projetaobcc20172.com.projetopetemfoco.R;
+
+/**
+ * Created by raul1 on 03/01/2018.
+ */
+
+public class BuscaEstabelecimentoActivity extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.activity_busca_estabelecimento, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Busca por nome estabelecimento");
+    }
+}

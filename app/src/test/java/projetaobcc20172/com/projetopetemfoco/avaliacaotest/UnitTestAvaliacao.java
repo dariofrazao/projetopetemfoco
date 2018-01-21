0c1f484d13012e2aa02649f466749e5fb99cbd1d
Alexsandro <alexsandroms@gmail.com>
2018-01-21 00:24:23 -0300

    Mudanç nos model (Endereco e Fornecedor)para a recuperação das coordenadas geograficas que mudaram para o nó endereço. Mudança na função que calcula a média da nota de avaliação, sendo trasferida para a avaliacaoDaoImpl. Mudança no avaliacaoDao, para realização das médias das notas. Mudança no avaliacaoDaoImpl, para realização das médias das notas. Mudança nas mensagens que atualização de nota. Correção Teste Unitário

--- app/src/test/java/projetaobcc20172/com/projetopetemfoco/avaliacaotest/UnitTestAvaliacao.java
+++ app/src/test/java/projetaobcc20172/com/projetopetemfoco/avaliacaotest/UnitTestAvaliacao.java
@@ -3,7 +3,9 @@ package projetaobcc20172.com.projetopetemfoco.avaliacaotest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
 import java.util.Arrays;
+
 import projetaobcc20172.com.projetopetemfoco.excecoes.CampoObrAusenteException;
 import projetaobcc20172.com.projetopetemfoco.model.Avaliacao;
 import projetaobcc20172.com.projetopetemfoco.utils.VerificadorDeObjetos;
@@ -14,44 +16,39 @@ import projetaobcc20172.com.projetopetemfoco.utils.VerificadorDeObjetos;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+@RunWith(Parameterized.class)
 public class UnitTestAvaliacao {
 
-    @RunWith(Parameterized.class)
-    public class UnitTestCadastroEndereco {
+    @Parameterized.Parameter
+    public Avaliacao endTest;
 
-        @Parameterized.Parameter
-        public Avaliacao endTest;
+    @Parameterized.Parameters
+    public static Iterable<?> data() {
+        String mIdUsuario = "05252878745878745";
+        String mNomeUsuario = "MMMMMMM GGGGGG DDDD";
+        int mEstrelas = 5;
+        String mCometario = "Excelente Estabelecimento";
 
-        @Parameterized.Parameters
-        public Iterable<?> data() {
+        return Arrays.asList(
+                new Avaliacao("", "", 0, ""),
+                new Avaliacao(mIdUsuario, mNomeUsuario, 0, ""),
+                new Avaliacao(mIdUsuario, mNomeUsuario, mEstrelas, ""),
+                new Avaliacao(mIdUsuario, mNomeUsuario, mEstrelas, mCometario),
+                new Avaliacao(mIdUsuario, "", mEstrelas, ""),
+                new Avaliacao(mIdUsuario, "", mEstrelas, mCometario),
+                new Avaliacao(mIdUsuario, "", 0, mCometario),
+                new Avaliacao("", mNomeUsuario, 0, ""),
+                new Avaliacao("", mNomeUsuario, mEstrelas, ""),
+                new Avaliacao("", mNomeUsuario, mEstrelas, mCometario),
+                new Avaliacao("", "", mEstrelas, ""),
+                new Avaliacao("", "", mEstrelas, mCometario),
+                new Avaliacao("", "", 0, mCometario),
+                new Avaliacao("", mNomeUsuario, 0, mCometario));
+    }
 
-            String mIdUsuario = "05252878745878745" ;
-            String mNomeUsuario = "MMMMMMM GGGGGG DDDD" ;
-            String mEstrelas = "5" ;
-            String mCometario = "Excelente Estabelecimento" ;
-
-            return Arrays.asList(
-                    new Avaliacao("", "", "", ""),
-                    new Avaliacao(mIdUsuario, mNomeUsuario, "", ""),
-                    new Avaliacao(mIdUsuario, mNomeUsuario, mEstrelas, ""),
-                    new Avaliacao(mIdUsuario, mNomeUsuario, mEstrelas, mCometario),
-                    new Avaliacao(mIdUsuario, "", mEstrelas, ""),
-                    new Avaliacao(mIdUsuario, "", mEstrelas, mCometario),
-                    new Avaliacao(mIdUsuario, "", "", mCometario),
-                    new Avaliacao("", mNomeUsuario, "", ""),
-                    new Avaliacao("", mNomeUsuario, mEstrelas, ""),
-                    new Avaliacao("", mNomeUsuario, mEstrelas, mCometario),
-                    new Avaliacao("", "", mEstrelas, ""),
-                    new Avaliacao("", "", mEstrelas, mCometario),
-                    new Avaliacao("", "", "", mCometario),
-                    new Avaliacao("", mNomeUsuario, "", mCometario)
-                    );
-        }
-
-        @Test(expected = CampoObrAusenteException.class)
-        public void testCampoObg() throws CampoObrAusenteException {
-            VerificadorDeObjetos.vDadosObjAvaliacao(endTest);
-        }
-
+    @Test(expected = CampoObrAusenteException.class)
+    public void testCampoObg() throws CampoObrAusenteException {
+        VerificadorDeObjetos.vDadosObjAvaliacao(endTest);
     }
 }

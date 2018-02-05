9d9c17eeabffaa16bc217777263d5b7d1bd137f2
Raul Sousa <raulpedrouag@gmail.com>
2018-02-05 01:57:29 -0300

    Add correções sugeridas pelo codacy.

--- app/src/main/java/projetaobcc20172/com/projetopetemfoco/adapter/PromocoesRecyclerViewAdapter.java
+++ app/src/main/java/projetaobcc20172/com/projetopetemfoco/adapter/PromocoesRecyclerViewAdapter.java
@@ -1,7 +1,6 @@
 package projetaobcc20172.com.projetopetemfoco.adapter;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
